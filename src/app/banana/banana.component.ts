1dffc838076f7919c17950329f73c44cea158048
riley909 <increaseblue666@gmail.com>
2021-09-12 22:10:04 +0900

    create banana.component.ts

---
+++ src/app/banana/banana.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'banana',
+  templateUrl: './banana.component.html',
+  styleUrls: ['./banana.component.css'],
+})
+export class BananaComponent {}
